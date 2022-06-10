86aca6b252a316bf8eddfcf26a84677b31274afe
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-10 18:18:00 +0300

    commit tree 4ed1d74496e1fba78ec1c61f1aed161bafd82bce

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+  return drivers.filter((match)=> match.toLowerCase()===string.toLowerCase());
+  
+}
+findMatching()
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(
+    (match) =>match.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+fuzzyMatch()
+
+function matchName(drivers, string){
+    return drivers.filter((match)=>match.name===string);
+}
